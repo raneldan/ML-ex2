2310a863b51c2519ce40fa97bc7990d0d09ee9b2
Ran <raneeldan@gmail.com>
2020-04-17 18:09:02 +0300

    fix removing files and added features vector creation for test data

--- data_parser.py
+++ data_parser.py
@@ -2,7 +2,10 @@ import csv
 from mnist import MNIST
 from pic_to_vector import PicToVec
 import os
-import glob
+import run_perceptron
+
+train_features_file_name = 'features/train_images.csv'
+test_features_file_name = 'features/test_images.csv'
 
 mndata = MNIST('data')
 
@@ -10,11 +13,18 @@ train_images, train_labels = mndata.load_training()
 
 test_images, test_labels = mndata.load_testing()
 
-files = glob.glob('/features')
-for f in files:
+filesToRemove = [os.path.join('features', f) for f in os.listdir('features')]
+for f in filesToRemove:
     os.remove(f)
 
-with open('features/train_images.csv', 'w', newline='', encoding='utf-8') as csvoutForFeatures:
-    csvoutForFeatures = csv.writer(csvoutForFeatures)
+with open(train_features_file_name, 'w', newline='', encoding='utf-8') as csvoutForTrainFeatures,\
+     open(test_features_file_name, 'w', newline='', encoding='utf-8') as csvoutForTestFeatures:
+    csvoutForTrainFeatures = csv.writer(csvoutForTrainFeatures)
+    csvoutForTestFeatures = csv.writer(csvoutForTestFeatures)
     for i in train_images:
-        csvoutForFeatures.writerow(PicToVec(i).format())
+        csvoutForTrainFeatures.writerow(PicToVec(i).format())
+    for i in test_images:
+        csvoutForTestFeatures.writerow(PicToVec(i).format())
+
+    #run_perceptron.run(train_features_file_name, test_features_file_name)
+
