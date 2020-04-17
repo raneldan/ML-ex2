bdc9323e01bc9cebfb7a8b0e3434684df85d0d91
Ran <raneeldan@gmail.com>
2020-04-17 17:45:36 +0300

    created vector of features from every input. added perceptron algo files

--- data_parser.py
+++ data_parser.py
@@ -1,10 +1,20 @@
+import csv
 from mnist import MNIST
 from pic_to_vector import PicToVec
+import os
+import glob
 
 mndata = MNIST('data')
 
 train_images, train_labels = mndata.load_training()
 
 test_images, test_labels = mndata.load_testing()
-for i in train_images:
-    print(PicToVec(i).format())
+
+files = glob.glob('/features')
+for f in files:
+    os.remove(f)
+
+with open('features/train_images.csv', 'w', newline='', encoding='utf-8') as csvoutForFeatures:
+    csvoutForFeatures = csv.writer(csvoutForFeatures)
+    for i in train_images:
+        csvoutForFeatures.writerow(PicToVec(i).format())
--- pic_to_vector.py
+++ pic_to_vector.py
@@ -1,5 +1,7 @@
 from typing import List
 
+dimension = 28
+
 class PicToVec:
     def __init__(self, pic: List[int]):
         self.pic: List[int] = pic
@@ -12,7 +14,8 @@ class PicToVec:
     def __init_funcs(self,):
         self.funcs.append(self.sum_of_values)
         self.funcs.append(self.num_of_pixels)
-
+        self.funcs.append(self.symmetry_x)
+        self.funcs.append(self.symmetry_y)
 
     def sum_of_values(self) -> int:
         sum = 0
@@ -26,6 +29,25 @@ class PicToVec:
             if (cell != 0):
                 counter += 1
         return counter
+
+    def symmetry_x(self):
+        score = 0
+        for index, cell in enumerate(self.pic):
+            if (index % dimension < dimension / 2):
+                if (cell != 0 and cell == self.pic[index+ int(dimension/2)]):
+                    score += 1
+        return score
+
+    def symmetry_y(self):
+        score = 0
+        for index, cell in enumerate(self.pic):
+            if index < dimension * dimension / 2:
+                if cell != 0 and cell == self.pic[self.calc_y_position(index)]:
+                    score += 1
+        return score
+
+    def calc_y_position(self, index):
+        return (index % dimension) + ((dimension - int(index/dimension) - 1) * dimension)
 
     def format(self):
         return self.vector
